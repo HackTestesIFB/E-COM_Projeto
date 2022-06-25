0b4e049541b28a0b11ee78c32f4b5c6dcd29bfaf
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-06-25 20:34:36 -0300

    Flutter app completo (sem revisão)

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -10,30 +10,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function CriaEntradaJogos(nome, descricao, imagem_link)
+function criaEntradaJogos(nome, descricao, imagem_link, valor)
 {
-    return {'Nome': nome, 'Descrição': descricao, 'Imagem_capa': imagem_link}
+    return {'Nome': nome, 'Descricao': descricao, 'Imagem_capa': imagem_link, 'Valor': valor}
 }
 
 jogos =
 [
-    CriaEntradaJogos('Halo: The Master Chief Collection', 'A lendária jornada do Master Chief está incluída em seis jogos, feitos para PC e reunidos em uma única experiência. Tanto para fãs de longa data como para os que conhecerão o Spartan 117 agora, a Master Chief Collection é a experiência de jogo definitiva da série Halo.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/976730/header.jpg?t=1649955774'),
+    criaEntradaJogos('Halo: The Master Chief Collection', 'A lendária jornada do Master Chief está incluída em seis jogos, feitos para PC e reunidos em uma única experiência. Tanto para fãs de longa data como para os que conhecerão o Spartan 117 agora, a Master Chief Collection é a experiência de jogo definitiva da série Halo.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/976730/header.jpg?t=1649955774', 75.0),
 
-    CriaEntradaJogos('Counter-Strike: Global Offensive', 'O Counter-Strike: Global Offensive (CS:GO) melhora a jogabilidade de ação baseada em equipes na qual foi pioneiro quando lançado há 19 anos. O CS:GO contém novos mapas, personagens e armas, além de contar com versões atualizadas de conteúdos do CS clássico (como de_dust2).', 'https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1641233427'),
+    criaEntradaJogos('Counter-Strike: Global Offensive', 'O Counter-Strike: Global Offensive (CS:GO) melhora a jogabilidade de ação baseada em equipes na qual foi pioneiro quando lançado há 19 anos. O CS:GO contém novos mapas, personagens e armas, além de contar com versões atualizadas de conteúdos do CS clássico (como de_dust2).', 'https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1641233427', 99.0),
 
-    CriaEntradaJogos('Half-Life: Alyx', 'Half-Life: Alyx é a volta da Valve à série Half-Life em realidade virtual. Trata-se da história de uma impossível luta contra uma raça alienígena cruel, conhecida como Combine, situada entre os eventos de Half-Life e Half-Life 2.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/546560/header.jpg?t=1641577012'),
+    criaEntradaJogos('Half-Life: Alyx', 'Half-Life: Alyx é a volta da Valve à série Half-Life em realidade virtual. Trata-se da história de uma impossível luta contra uma raça alienígena cruel, conhecida como Combine, situada entre os eventos de Half-Life e Half-Life 2.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/546560/header.jpg?t=1641577012', 199.0),
 
-    CriaEntradaJogos('Batman™: Arkham Knight', 'Batman™: Arkham Knight é a conclusão épica da trilogia Arkham da Rocksteady Studios. Desenvolvido exclusivamente para plataformas de última geração, Batman™: Arkham Knight apresenta o design exclusivo da Rocksteady para o Batmóvel.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/208650/header.jpg?t=1634156452'),
+    criaEntradaJogos('Batman™: Arkham Knight', 'Batman™: Arkham Knight é a conclusão épica da trilogia Arkham da Rocksteady Studios. Desenvolvido exclusivamente para plataformas de última geração, Batman™: Arkham Knight apresenta o design exclusivo da Rocksteady para o Batmóvel.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/208650/header.jpg?t=1634156452', 250.0),
 
-    CriaEntradaJogos('Grand Theft Auto V', 'Grand Theft Auto V para PC oferece aos jogadores a opção de explorar o gigantesco e premiado mundo de Los Santos e Blaine County em resoluções de até 4K e além, assim como a chance de experimentar o jogo rodando a 60 FPS (quadros por segundo).', 'https://cdn.cloudflare.steamstatic.com/steam/apps/271590/header.jpg?t=1618856444'),
+    criaEntradaJogos('Grand Theft Auto V', 'Grand Theft Auto V para PC oferece aos jogadores a opção de explorar o gigantesco e premiado mundo de Los Santos e Blaine County em resoluções de até 4K e além, assim como a chance de experimentar o jogo rodando a 60 FPS (quadros por segundo).', 'https://cdn.cloudflare.steamstatic.com/steam/apps/271590/header.jpg?t=1618856444', 120.0),
 
-    CriaEntradaJogos('Grand Theft Auto IV: The Complete Edition', 'Niko Bellic, Johnny Klebitz e Luis Lopez têm uma coisa em comum: eles vivem na pior cidade da América do Norte. Liberty City cultua dinheiro e status, e é o paraíso para quem tem essas coisas, mas um pesadelo para quem não tem.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/12210/header.jpg?t=1618853493'),
+    criaEntradaJogos('Grand Theft Auto IV: The Complete Edition', 'Niko Bellic, Johnny Klebitz e Luis Lopez têm uma coisa em comum: eles vivem na pior cidade da América do Norte. Liberty City cultua dinheiro e status, e é o paraíso para quem tem essas coisas, mas um pesadelo para quem não tem.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/12210/header.jpg?t=1618853493', 99.0),
 
-    CriaEntradaJogos('Ready or Not', 'Ready or Not é um jogo de tiro em primeira pessoa, intenso, tático e atual. Nele, as unidades policiais da SWAT são chamadas para controlar situações de hostilidade e de confronto.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/1144200/header.jpg?t=1655539544'),
+    criaEntradaJogos('Ready or Not', 'Ready or Not é um jogo de tiro em primeira pessoa, intenso, tático e atual. Nele, as unidades policiais da SWAT são chamadas para controlar situações de hostilidade e de confronto.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/1144200/header.jpg?t=1655539544', 50.0),
 ]
 
-app.get('/GetJogos/', async (req, res) =>
+app.get('/GetJogos', async (req, res) =>
 {
+    console.log('Get - /GetJogos');
     try
     {
         res.json
@@ -55,17 +56,18 @@ app.get('/GetJogos/', async (req, res) =>
 
 app.post('/RealizarCompra', async (req, res) =>
 {
-    try
-    {
+    console.log('Post - /RealizarCompra', req.body);
+   // try
+    //{
         res.json
         ({
             Status: 'OK',
             Descricao: 'Compra realizada',
             Requisicao: req.body
         })
-    }
+    //}
 
-    catch(error)
+    /*catch(error)
     {
         res.json
         ({
@@ -74,7 +76,7 @@ app.post('/RealizarCompra', async (req, res) =>
             Requisicao: req.body,
             Erro: error
         });
-    }
+    }*/
 });
 
 
--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -2,118 +2,115 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-var url = http.get(Uri.http('10.0.2.2:8080', '/teste/listarbonecos'));
+var url = http.get(Uri.http('127.0.0.1:8000', '/GetJogos'));
 
-void main() {
-  runApp(const MyApp());
+Future listarJogos() async
+{
+    return await http.get(Uri.http('localhost:8000', '/GetJogos'));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+Future comprarJogos(String nome_do_jogo) async
+{
+    return await http.post(Uri.http('localhost:8000', '/RealizarCompra'), headers: {"Content-Type": "application/json"}, body: json.encode({'Nome': '$nome_do_jogo'}));
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class Jogo
+{
+    final String _nome;
+    final String _descricao;
+    final String _foto;
+    final double _valor;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    String get nome => _nome;
+    String get descricao => _descricao;
+    String get foto => _foto;
+    double get valor => _valor;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    Jogo( this._nome, this._descricao, this._foto, this._valor);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+    Jogo.fromJson(Map json) : _nome=json['Nome'], _descricao=json['Descricao'], _foto=json['Imagem_capa'], _valor=json['Valor'];
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+void main() => runApp(const MeuApp());
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MeuApp extends StatelessWidget
+{
+    const MeuApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    @override
+    build(context)
+    {
+        return const MaterialApp
+        (
+            title: 'Loja de jogos',
+            home: HomePage(),
+        );
+    }
+}
+
+class HomePage extends StatefulWidget 
+{
+    const HomePage({Key? key}) : super(key: key);
+
+    @override
+    State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage>
+{
+  var jogos=[];
+ 
+    HomePageState()
+    {
+        listarJogos().then((response)
+        {
+            setState(()
+            {
+                Iterable lista = json.decode(response.body)['Jogos'];
+                jogos = json.decode(response.body)['Jogos'];// lista.map((model) => Jogo.fromJson(model)).toList();
+            });
+        });
+    }
+  
+    @override
+    Widget build(BuildContext context)
+    {
+        return Material
+        (
+            child: ListView.builder
+            (
+                itemCount : jogos.length,
+                itemBuilder: (context, index)
+                {
+                    return ListTile
+                    (
+                        title: Text
+                        (
+                            jogos[index]['Nome'],
+                            style: const TextStyle
+                            (
+                                fontSize: 20,
+                                color: Colors.black
+                            )
+                        ),
+
+                        subtitle: Text(jogos[index]['Descricao'].toString().substring(0, 30)+'...'),
+
+                        trailing: FloatingActionButton
+                        (
+                            child: Icon(Icons.add_shopping_cart),
+                            onPressed: ()
+                            {
+                                comprarJogos(jogos[index]['Nome']).then((response)
+                                {
+                                    dynamic resultado = response.body;
+                                    print('Resultado = ${resultado}');
+                                });
+                            }
+                        )
+                    );
+                },
+            )
+        );
+    }
 }
