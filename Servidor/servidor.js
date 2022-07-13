3c846375e246a5a641901d6bc7f9515e96fa6ee4
HackTestesIFB <caio.flausino@estudante.ifb.edu.br>
2022-07-13 10:57:13 -0300

    HashMap, carrinho 100%

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -27,7 +27,7 @@ function criaEntradaJogos(nome, descricao, imagem_link, valor)
 }
 
 // Lista de objetos --> posteriormente podemos utilizar um HashMap para facilitar as consultas (HashMap --> array)
-jogos =
+/*jogos =
 [
     criaEntradaJogos('Halo: The Master Chief Collection', 'A lendária jornada do Master Chief está incluída em seis jogos, feitos para PC e reunidos em uma única experiência. Tanto para fãs de longa data como para os que conhecerão o Spartan 117 agora, a Master Chief Collection é a experiência de jogo definitiva da série Halo.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/976730/header.jpg?t=1649955774', 75.0),
 
@@ -42,7 +42,24 @@ jogos =
     criaEntradaJogos('Grand Theft Auto IV: The Complete Edition', 'Niko Bellic, Johnny Klebitz e Luis Lopez têm uma coisa em comum: eles vivem na pior cidade da América do Norte. Liberty City cultua dinheiro e status, e é o paraíso para quem tem essas coisas, mas um pesadelo para quem não tem.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/12210/header.jpg?t=1618853493', 99.0),
 
     criaEntradaJogos('Ready or Not', 'Ready or Not é um jogo de tiro em primeira pessoa, intenso, tático e atual. Nele, as unidades policiais da SWAT são chamadas para controlar situações de hostilidade e de confronto.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/1144200/header.jpg?t=1655539544', 50.0),
-]
+]*/
+
+jogos = new Map();
+
+    jogos.set('Halo: The Master Chief Collection', criaEntradaJogos('Halo: The Master Chief Collection', 'A lendária jornada do Master Chief está incluída em seis jogos, feitos para PC e reunidos em uma única experiência. Tanto para fãs de longa data como para os que conhecerão o Spartan 117 agora, a Master Chief Collection é a experiência de jogo definitiva da série Halo.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/976730/header.jpg?t=1649955774', 75.0));
+
+    jogos.set('Counter-Strike: Global Offensive', criaEntradaJogos('Counter-Strike: Global Offensive', 'O Counter-Strike: Global Offensive (CS:GO) melhora a jogabilidade de ação baseada em equipes na qual foi pioneiro quando lançado há 19 anos. O CS:GO contém novos mapas, personagens e armas, além de contar com versões atualizadas de conteúdos do CS clássico (como de_dust2).', 'https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg?t=1641233427', 99.0));
+
+    jogos.set('Half-Life: Alyx', criaEntradaJogos('Half-Life: Alyx', 'Half-Life: Alyx é a volta da Valve à série Half-Life em realidade virtual. Trata-se da história de uma impossível luta contra uma raça alienígena cruel, conhecida como Combine, situada entre os eventos de Half-Life e Half-Life 2.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/546560/header.jpg?t=1641577012', 199.0));
+
+    jogos.set('Batman™: Arkham Knight', criaEntradaJogos('Batman™: Arkham Knight', 'Batman™: Arkham Knight é a conclusão épica da trilogia Arkham da Rocksteady Studios. Desenvolvido exclusivamente para plataformas de última geração, Batman™: Arkham Knight apresenta o design exclusivo da Rocksteady para o Batmóvel.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/208650/header.jpg?t=1634156452', 250.0));
+
+    jogos.set('Grand Theft Auto V', criaEntradaJogos('Grand Theft Auto V', 'Grand Theft Auto V para PC oferece aos jogadores a opção de explorar o gigantesco e premiado mundo de Los Santos e Blaine County em resoluções de até 4K e além, assim como a chance de experimentar o jogo rodando a 60 FPS (quadros por segundo).', 'https://cdn.cloudflare.steamstatic.com/steam/apps/271590/header.jpg?t=1618856444', 120.0));
+
+    jogos.set('Grand Theft Auto IV: The Complete Edition', criaEntradaJogos('Grand Theft Auto IV: The Complete Edition', 'Niko Bellic, Johnny Klebitz e Luis Lopez têm uma coisa em comum: eles vivem na pior cidade da América do Norte. Liberty City cultua dinheiro e status, e é o paraíso para quem tem essas coisas, mas um pesadelo para quem não tem.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/12210/header.jpg?t=1618853493', 99.0));
+
+    jogos.set('Ready or Not', criaEntradaJogos('Ready or Not', 'Ready or Not é um jogo de tiro em primeira pessoa, intenso, tático e atual. Nele, as unidades policiais da SWAT são chamadas para controlar situações de hostilidade e de confronto.', 'https://cdn.cloudflare.steamstatic.com/steam/apps/1144200/header.jpg?t=1655539544', 50.0));
+
 
 // Envia todos os jogos da loja
 app.get('/getJogos', async (req, res) =>
@@ -53,7 +70,7 @@ app.get('/getJogos', async (req, res) =>
         res.json
         ({
             Status: 'OK',
-            Jogos: jogos,
+            Jogos: [...jogos.values()],
         })
     }
 
@@ -97,7 +114,7 @@ app.post('/postCarrinho', async (req, res) => {
     console.log('Post - /postCarrinho');
     const { idUsuario, idProduto, quantidade } = req.body;
     try {
-        let precoProduto = jogos[idProduto].Valor;
+        let precoProduto = jogos.get(idProduto)['Valor'];
         const usuario = await Carrinho.create({ idUsuario, idProduto, precoProduto, quantidade });
         res.status(201).json({msg: 'Adicionado ao carrinho com sucesso.'});
     } catch(err) {
